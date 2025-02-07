5804901775c7cf55c0b3354e56403dc643e4c3b4
mkh <developermekh@gmail.com>
2025-02-07 22:04:29 +0200

    spotless with googleJavaFormat style

--- backoffice-api/src/main/java/ua/backofficeapi/BackofficeApiApplication.java
+++ backoffice-api/src/main/java/ua/backofficeapi/BackofficeApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackofficeApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackofficeApiApplication.class, args);
-    }
+  public static void main(String[] args) {
 
+    SpringApplication.run(BackofficeApiApplication.class, args);
+  }
 }
