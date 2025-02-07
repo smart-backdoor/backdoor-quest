0d760ef15e529054cf42ac39af1401a0758b6595
mkh <developermekh@gmail.com>
2025-02-07 21:47:46 +0200

    init BE projects & infra

---
+++ backoffice-api/src/main/java/ua/backofficeapi/BackofficeApiApplication.java
@@ -0,0 +1,13 @@
+package ua.backofficeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackofficeApiApplication.class, args);
+    }
+
+}
---
+++ quest-api/src/main/java/ua/questapi/QuestApiApplication.java
@@ -0,0 +1,13 @@
+package ua.questapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestApiApplication.class, args);
+    }
+
+}
