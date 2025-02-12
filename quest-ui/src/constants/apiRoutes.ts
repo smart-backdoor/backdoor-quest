439b54faa5370abb3c605ad59ef13bfd2d3a68c6
Yekateryna Sukhodolska <yekateryna.s@delasport.com>
2025-02-13 01:25:33 +0200

    fix: add route

--- quest-ui/src/constants/apiRoutes.ts
+++ quest-ui/src/constants/apiRoutes.ts
@@ -1,10 +1,13 @@
 export const API = Object.freeze({
   AUTH: {
-    LOGIN: 'http://localhost:8080/auth/login',
-    REGISTER: 'http://localhost:8080/auth/register',
+    LOGIN: 'https://9784-176-37-182-123.ngrok-free.app/auth/login',
+    REGISTER: 'https://9784-176-37-182-123.ngrok-free.app/auth/register',
   },
-  USER: 'http://localhost:8080/user/profile/:id',
-  ALL_QUESTS: 'http://localhost:8080/quests',
-  START_QUEST: 'http://localhost:8080/quests/:id/start',
-  VALIDATE_QUEST: 'http://localhost:8080/quests/:id/validate',
+  USER: 'https://9784-176-37-182-123.ngrok-free.app/user/profile/:id',
+  QUEST_COMPLETE:
+    'https://9784-176-37-182-123.ngrok-free.app/quests/:id/complete',
+  ALL_QUESTS: 'https://9784-176-37-182-123.ngrok-free.app/quests',
+  START_QUEST: 'https://9784-176-37-182-123.ngrok-free.app/quests/:id/start',
+  VALIDATE_QUEST:
+    'https://9784-176-37-182-123.ngrok-free.app/quests/:id/validate',
 });
